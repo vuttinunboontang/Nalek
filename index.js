6f621b47e147e70ce2ef6e9bff6f94ed86f46d9e
vuttinunboontang <48982674+vuttinunboontang@users.noreply.github.com>
2023-07-05 14:43:58 +0700

    index.js

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 async function main() {
-    liff.ready.then(() => {
-        if (liff.getOS() === "ios") {
-            
-        }
-        if (liff.isInClient()) {
-            getUserProfile()
-        }
-    })
+  liff.ready.then(() => {
+      if (liff.getOS() === "ios") {
+          
+      }
+      if (liff.isInClient()) {
+          getUserProfile()
+      }
+  })
 
 
-    await liff.init({liffId: "2000030741-2WGZeb9Z", withLoginOnExternalBrowser: true })
+  await liff.init({liffId: "2000030741-2WGZeb9Z", withLoginOnExternalBrowser: true })
 }
 main()
 
 async function getUserProfile() {
-    const profile = await liff.getProfile()
-    document.getElementById("pictureUrl").src = profile.pictureUrl
-    document.getElementById("userId").append(profile.userId)
-    document.getElementById("statusMessage").append(profile.statusMessage)
-    document.getElementById("displayName").append(profile.displayName)
-    document.getElementById("docodedIDToken").append(liff.getDecodedIDToken().email)
-  }
+  const profile = await liff.getProfile()
+  document.getElementById("pictureUrl").src = profile.pictureUrl
+  document.getElementById("userId").append(profile.userId)
+  document.getElementById("statusMessage").append(profile.statusMessage)
+  document.getElementById("displayName").append(profile.displayName)
+  document.getElementById("docodedIDToken").append(liff.getDecodedIDToken().email)
+}
